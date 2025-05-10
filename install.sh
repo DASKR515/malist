30af0d98d7013b06e8d892ceb0e85de56a49d954
DASKR <167687177+DASKR515@users.noreply.github.com>
2025-05-10 17:23:43 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 echo "[*] Installing Node.js..."
 pkg install -y nodejs git
 
-echo "[*] Cloning the anime-cli project..."
-git clone https://github.com/YOUR_USERNAME/anime-cli.git ~/anime-cli
+echo "[*] Cloning the malist project..."
+git clone https://github.com/DASKR515/malist.git ~/malist
 
 echo "[*] Installing JavaScript dependencies..."
-cd ~/anime-cli
+cd ~/malist
 npm install
 
 echo "[*] Creating 'malist' command..."
-echo -e "#!/bin/bash\nnode ~/anime-cli/main.js" > $PREFIX/bin/malist
+echo -e "#!/bin/bash\nnode ~/malist/main.js" > $PREFIX/bin/malist
 chmod +x $PREFIX/bin/malist
 
 echo "[+] Done! Now you can type 'malist' in your terminal!"
